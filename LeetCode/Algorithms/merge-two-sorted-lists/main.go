5d19c89a8a379e3da5bce51689bb3b3800704fbc
Huanghz <shmily@miku.tw>
2019-03-27 21:54:29 +0800

    2019年3月27日

--- LeetCode/Algorithms/merge-two-sorted-lists/main.go
+++ LeetCode/Algorithms/merge-two-sorted-lists/main.go
@@ -20,11 +20,18 @@ type ListNode struct {
 }
 
 // 注意为有序
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+func mergeTwoLists1(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	} else if l1 != nil && l2 == nil {
+		return l1
+	} else if l1 == nil && l2 != nil {
+		return l2
+	}
 	intList := make([]int, 0, 8)
 	re := &ListNode{}
 	tmp := re
-	for l1.Next != nil && l2.Next != nil {
+	for l1.Next != nil || l2.Next != nil {
 		if l1.Next != nil {
 			intList = append(intList, l1.Val)
 			l1 = l1.Next
@@ -35,17 +42,44 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 	}
 	intList = append(intList, l1.Val, l2.Val)
+
 	sort.Ints(intList) // 排序
+	//判断
+
 	for i := 0; i < len(intList); i++ {
-		tmp = &ListNode{Val: intList[i]}
-		tmp.Next = &ListNode{}
-		re.Next = tmp
+		//第一个ListNode
+		tmp.Val = intList[i]
+		if i+1 != len(intList) {
+			tmp.Next = &ListNode{}
+			tmp = tmp.Next
+		}
 
 	}
-	return re.Next
+	return re
+}
+
+/**
+利用递归
+*/
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil { //明确的终止条件
+		return l2
+	}
+	if l2 == nil { //明确的终止条件
+		return l1
+	}
+	var res *ListNode
+	if l1.Val >= l2.Val {
+		res = l2
+		res.Next = mergeTwoLists(l1, l2.Next)
+	} else {
+		res = l1
+		res.Next = mergeTwoLists(l1.Next, l2)
+	}
+	return res
 }
 func main() {
-	l1 := &ListNode{1, &ListNode{2, &ListNode{Val: 4}}}
-	l2 := &ListNode{1, &ListNode{3, &ListNode{Val: 4}}}
+	l1 := &ListNode{Val: 5}
+	l2 := &ListNode{1, &ListNode{2, &ListNode{Val: 4}}}
 	fmt.Println(mergeTwoLists(l1, l2))
 }
