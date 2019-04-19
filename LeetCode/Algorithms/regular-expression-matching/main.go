3a8bf729c556a5eb3cf3c7b5759ed4767a4374f7
hzHuang <shmily@miku.tw>
2019-04-19 12:00:18 +0800

    2019.04.19

--- LeetCode/Algorithms/regular-expression-matching/main.go
+++ LeetCode/Algorithms/regular-expression-matching/main.go
@@ -48,7 +48,44 @@ Output: false
 
 */
 func isMatch(s string, p string) bool {
+	dp := make([][]bool, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[0][0] = true //当 s长度0 p得长度为0 的时候,肯定为true
+	// 处理 "" 可以与 "a*b*c"匹配
+	for j := 1; j < len(p) && dp[0][j-1]; j += 2 {
+		if p[j] == '*' {
+			dp[0][j+1] = true
+		}
+	}
 
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(p); j++ {
+			if p[j] == '.' || p[j] == s[i] {
+				/* p[j] 与 s[i] 可以匹配上，所以，只要前面匹配，这里就能匹配上 */
+				dp[i+1][j+1] = dp[i][j]
+			} else if p[j] == '*' {
+				/* 此时，p[j] 的匹配情况与 p[j-1] 的内容相关。 */
+				if p[j-1] != s[i] && p[j-1] != '.' {
+					/**
+					 * p[j] 无法与 s[i] 匹配上
+					 * p[j-1:j+1] 只能被当做 ""
+					 */
+					dp[i+1][j+1] = dp[i+1][j-1]
+
+				} else {
+					/**
+					 * p[j] 与 s[i] 匹配上
+					 * p[j-1;j+1] 作为 "x*", 可以有三种解释
+					 */
+					dp[i+1][j+1] = dp[i+1][j-1] /* "x*" 解释为 "" */ || dp[i+1][j] /* "x*" 解释为 "x" */ || dp[i][j+1] /* "x*" 解释为 "x" */
+
+				}
+			}
+		}
+	}
+	return dp[len(s)][len(p)]
 }
 func main() {
 
