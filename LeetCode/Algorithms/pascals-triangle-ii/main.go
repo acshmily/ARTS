bbb601e31d5f14913e92ed02888dc6f958cc8676
acshmily <shmilytq@gmail.com>
2019-05-15 09:06:43 +0800

    2019年05月15日

---
+++ LeetCode/Algorithms/pascals-triangle-ii/main.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+Follow up:
+*/
+
+func getRow(rowIndex int) []int {
+	re := make([]int, rowIndex+1)
+	re[0] = 1
+	for i := 1; i < rowIndex; i++ {
+		for j := i; j >= 1; j-- {
+			re[j] = re[j] + re[j-1]
+		}
+	}
+	return re
+}
